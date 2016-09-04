106ef66bbfddc05385b397699a37911c8cc34e7d
Michael Ashby <ashby.michael@gmail.com>
2016-09-03 23:38:53 -0700

    add swim module

--- animals/penguin.rb
+++ animals/penguin.rb
@@ -1,18 +1,17 @@
 require_relative 'bird'
 require_relative 'egg'
+require_relative 'swim'
 
 class Penguin < Bird
+  include Swim
   attr_reader :favorite_food
 
   def initialize(args = {})
     @favorite_food = args[:favorite_food] || 'fish'
+    super
   end
 
   def reproduce
     Egg.new(animal: Penguin)
-  end
-
-  def swim
-    puts "I know how to swim!"
   end
 end
