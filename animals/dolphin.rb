45aa56f68034687358b8d3686ae892972ca0e558
Michael Ashby <ashby.michael@gmail.com>
2016-09-05 22:08:42 -0700

    fix dolphin typo

--- animals/dolphin.rb
+++ animals/dolphin.rb
@@ -1,12 +1,14 @@
-require_relative 'animal'
-
-class Dolphin < Animal
+class Dolphin
   attr_reader :cover, :movement, :favorite_food
 
   def initialize(args = {})
     @cover = args[:cover] || 'skin'
-    @movement = args[:movement] || 'fin'
+    @movement = args[:movement] || 'fins'
     @favorite_food = args[:favorite_food] || 'squid'
+  end
+
+  def eat
+    puts "I ate some #{@favorite_food}!"
   end
 
   def reproduce
