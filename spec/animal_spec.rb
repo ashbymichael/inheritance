632d1ca05d8095244711e31a38b8986f21529109
Michael Ashby <ashby.michael@gmail.com>
2016-09-05 22:01:47 -0700

    add penguin and pigeon tests

--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -29,7 +29,7 @@ describe Bat do
   end
 
   it "can produce live young" do
-    baby_bat = Bat.new
+    baby_bat = bat.reproduce
     expect(baby_bat).to be_a(Bat)
   end
 
@@ -39,17 +39,18 @@ describe Bat do
   end
 
 end
+
 describe Dolphin do
-let(:dolphin) { Dolphin.new }
+  let(:dolphin) { Dolphin.new }
 
-it "has default values for all attributes" do
-  expect(dolphin.cover).to eq('skin')
-  expect(dolphin.movement).to eq('fins')
-  expect(dolphin.favorite_food).to eq('squid')
-end
+  it "has default values for all attributes" do
+    expect(dolphin.cover).to eq('skin')
+    expect(dolphin.movement).to eq('fins')
+    expect(dolphin.favorite_food).to eq('squid')
+  end
 
-it "can assign new values to any attribute" do
-  dolphin_hash = {cover: 'scales',
+  it "can assign new values to any attribute" do
+    dolphin_hash = {cover: 'scales',
     movement: 'tentacles',
     favorite_food: 'houses'}
     hash_dolphin = Dolphin.new(dolphin_hash)
@@ -65,7 +66,7 @@ it "can assign new values to any attribute" do
   end
 
   it "can produce live young" do
-    baby_dolphin = Dolphin.new
+    baby_dolphin = dolphin.reproduce
     expect(baby_dolphin).to be_a(Dolphin)
   end
 
@@ -73,4 +74,84 @@ it "can assign new values to any attribute" do
     expect(STDOUT).to receive(:puts).with("I know how to swim!")
     dolphin.swim
   end
+end
+
+describe Penguin do
+  let(:penguin) { Penguin.new }
+
+  it "has default values for all attributes" do
+    expect(penguin.cover).to eq('feathers')
+    expect(penguin.movement).to eq('wings')
+    expect(penguin.favorite_food).to eq('fish')
+  end
+
+  it "can assign new values to any attribute" do
+    penguin_hash = {cover: 'scales',
+    movement: 'tentacles',
+    favorite_food: 'houses'}
+    hash_penguin = Penguin.new(penguin_hash)
+
+    expect(hash_penguin.cover).to eq(penguin_hash[:cover])
+    expect(hash_penguin.movement).to eq(penguin_hash[:movement])
+    expect(hash_penguin.favorite_food).to eq(penguin_hash[:favorite_food])
+  end
+
+  it "can eat" do
+    expect(STDOUT).to receive(:puts).with("I ate some #{penguin.favorite_food}!")
+    penguin.eat
+  end
+
+  it "can lay an egg" do
+    baby_penguin = penguin.reproduce
+    expect(baby_penguin).to be_a(Egg)
+    baby_penguin = baby_penguin.hatch
+    expect(baby_penguin).to be_a(Penguin)
+  end
+
+  it "can swim" do
+    expect(STDOUT).to receive(:puts).with("I know how to swim!")
+    penguin.swim
+  end
+end
+
+describe Pigeon do
+  let(:pigeon) { Pigeon.new }
+
+  it "has default values for all attributes" do
+    expect(pigeon.cover).to eq('feathers')
+    expect(pigeon.movement).to eq('wings')
+    expect(pigeon.favorite_food).to eq('trash')
+    expect(pigeon.flight_distance).to eq('25km')
+  end
+
+  it "can assign new values to any attribute" do
+    pigeon_hash = {cover: 'leaves',
+                movement: 'motorcycle',
+                favorite_food: 'feet',
+                flight_distance: '18'}
+    hash_pigeon = Pigeon.new(pigeon_hash)
+
+    expect(hash_pigeon.cover).to eq(pigeon_hash[:cover])
+    expect(hash_pigeon.movement).to eq(pigeon_hash[:movement])
+    expect(hash_pigeon.favorite_food).to eq(pigeon_hash[:favorite_food])
+    expect(hash_pigeon.flight_distance).to eq(pigeon_hash[:flight_distance])
+  end
+
+  it "can eat" do
+    expect(STDOUT).to receive(:puts).with("I ate some #{pigeon.favorite_food}!")
+    pigeon.eat
+  end
+
+  it "can lay an egg" do
+    baby_pigeon = pigeon.reproduce
+    expect(baby_pigeon).to be_a(Egg)
+    baby_pigeon = baby_pigeon.hatch
+    expect(baby_pigeon).to be_a(Pigeon)
+  end
+
+  it "can fly" do
+    expect(STDOUT).to receive(:puts).with("I just flew #{pigeon.flight_distance}!")
+    pigeon.fly
+  end
+
 end
